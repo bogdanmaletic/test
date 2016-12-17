61d9e9e75c6697d73d51992ec4b7924c20ae95a0
komp <komp@kompprod64>
2016-12-17 19:06:50 +0100

    Ovo je naknadni test na caletovoj masini....

---
+++ TestGut/src/com/testStruct/MeshStruct.java
@@ -0,0 +1,18 @@
+package com.testStruct;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class MeshStruct extends LinkedList<BasicCell> {
+
+	public MeshStruct() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MeshStruct(Collection<? extends BasicCell> arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
