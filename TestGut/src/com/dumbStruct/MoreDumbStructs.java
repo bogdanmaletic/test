921ffc54c157cd91f9d2579da2645464771eab15
bogile <gosubloom@gmail.com>
2017-06-04 19:18:16 +0200

    Aj da vidimo da li mozemo da setamo

---
+++ TestGut/src/com/dumbStruct/MoreDumbStructs.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.dumbStruct;
+
+/**
+ * @author komp
+ *
+ */
+public class MoreDumbStructs extends DumbStruct implements Runnable {
+
+	/**
+	 * @param xPos
+	 * @param yPos
+	 * @param height
+	 * @param signalStrength
+	 */
+	public MoreDumbStructs(int xPos, int yPos, int height, double signalStrength) {
+		super(xPos, yPos, height, signalStrength);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
